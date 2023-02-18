292b2ed1503369947e4aa0bdf707515fa09d2b04
Luc Vermeer <l.vermeer@live.nl>
2023-02-18 18:46:18 +0100

    fixed bug in grade_record.py, passes locally

--- swagger_server/models/grade_record.py
+++ swagger_server/models/grade_record.py
@@ -14,25 +14,25 @@ class GradeRecord(Model):
 
     Do not edit the class manually.
     """
-    def __init__(self, subject_name: str=None, grade_record: float=None):  # noqa: E501
+    def __init__(self, subject_name: str=None, grade: float=None):  # noqa: E501
         """GradeRecord - a model defined in Swagger
 
         :param subject_name: The subject_name of this GradeRecord.  # noqa: E501
         :type subject_name: str
-        :param grade_record: The grade_record of this GradeRecord.  # noqa: E501
-        :type grade_record: float
+        :param grade: The grade of this GradeRecord.  # noqa: E501
+        :type grade: float
         """
         self.swagger_types = {
             'subject_name': str,
-            'grade_record': float
+            'grade': float
         }
 
         self.attribute_map = {
             'subject_name': 'subject_name',
-            'grade_record': 'gradeRecord'
+            'grade': 'grade'
         }
         self._subject_name = subject_name
-        self._grade_record = grade_record
+        self._grade = grade
 
     @classmethod
     def from_dict(cls, dikt) -> 'GradeRecord':
@@ -49,6 +49,7 @@ class GradeRecord(Model):
     def subject_name(self) -> str:
         """Gets the subject_name of this GradeRecord.
 
+        Subject Name  # noqa: E501
 
         :return: The subject_name of this GradeRecord.
         :rtype: str
@@ -59,6 +60,7 @@ class GradeRecord(Model):
     def subject_name(self, subject_name: str):
         """Sets the subject_name of this GradeRecord.
 
+        Subject Name  # noqa: E501
 
         :param subject_name: The subject_name of this GradeRecord.
         :type subject_name: str
@@ -69,22 +71,24 @@ class GradeRecord(Model):
         self._subject_name = subject_name
 
     @property
-    def grade_record(self) -> float:
-        """Gets the grade_record of this GradeRecord.
+    def grade(self) -> float:
+        """Gets the grade of this GradeRecord.
 
 
-        :return: The grade_record of this GradeRecord.
+        :return: The grade of this GradeRecord.
         :rtype: float
         """
-        return self._grade_record
+        return self._grade
 
-    @grade_record.setter
-    def grade_record(self, grade_record: float):
-        """Sets the grade_record of this GradeRecord.
+    @grade.setter
+    def grade(self, grade: float):
+        """Sets the grade of this GradeRecord.
 
 
-        :param grade_record: The grade_record of this GradeRecord.
-        :type grade_record: float
+        :param grade: The grade of this GradeRecord.
+        :type grade: float
         """
+        if grade is None:
+            raise ValueError("Invalid value for `grade`, must not be `None`")  # noqa: E501
 
-        self._grade_record = grade_record
+        self._grade = grade
